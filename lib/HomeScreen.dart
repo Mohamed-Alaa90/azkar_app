d932c720dd42477965cefc88230e68a1c88e8390
Mohamed-Alaa90 <m7md3laa500@gmail.com>
2024-03-06 22:52:59 +0200

    add_project

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  List
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'ازكاري',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: const Color(0xff005B4A),
+      ),
+      body: ,
+    );
+  }
+}
